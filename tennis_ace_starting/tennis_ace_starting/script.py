836db4d17e1e9966f741123554527857815b583a
Michal <mikubina@gmail.com>
2024-10-07 01:09:39 +0200

    Tennis Winning project
    Linear regression exercis

---
+++ tennis_ace_starting/tennis_ace_starting/script.py
@@ -0,0 +1,82 @@
+import codecademylib3_seaborn
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+
+# load and investigate the data here:
+
+df = pd.read_csv("tennis_stats.csv")
+print(df.head())
+
+
+# perform exploratory analysis here:
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+## perform single feature linear regressions here:
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+## perform two feature linear regressions here:
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+## perform multiple feature linear regressions here:
